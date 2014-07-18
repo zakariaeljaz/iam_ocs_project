cfe931300dc0d050ff93b7a007b38381576159e0
anasameur <basma@new-host-2.home>
2014-07-18 09:58:33 +0000

    closed ticket

--- close_ticket.php
+++ close_ticket.php
@@ -2,7 +2,7 @@
 session_start();
 //var_dump($_SESSION['user_id']);exit();
 include('bdd.php');
-$req = $bdd->prepare('UPDATE MS_CS SET taken_by = :taken_by WHERE id = :id');
+$req = $bdd->prepare('UPDATE MS_CS SET taken_by = :taken_by, Current_statut = \'Closed\' WHERE id = :id');
 $req->execute(array(
 	'taken_by'=>-1,
 	'id'=>$_GET['id']
