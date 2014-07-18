82c5f930918843cfc25125f83faa33c08b37f01d
anasameur <basma@new-host-2.home>
2014-07-18 10:15:01 +0000

    pending

--- take_ticket.php
+++ take_ticket.php
@@ -2,7 +2,7 @@
 session_start();
 //var_dump($_SESSION['user_id']);exit();
 include('bdd.php');
-$req = $bdd->prepare('UPDATE MS_CS SET taken_by = :taken_by WHERE id = :id');
+$req = $bdd->prepare('UPDATE MS_CS SET taken_by = :taken_by, Current_statut =\'Work in progress\' WHERE id = :id');
 $req->execute(array(
 	'taken_by'=>$_SESSION['user_id'],
 	'id'=>$_GET['id']
